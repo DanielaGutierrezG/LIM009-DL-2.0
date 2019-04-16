b0b8cfa8e7afd47924030671d015e634964153ab
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-04-16 13:07:32 -0500

    Pintando personajes en la web con algunas de sus caracteristicas

--- src/data.js
+++ src/data.js
@@ -2,3 +2,31 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
+// const contenedor = document.getElementById("contenedor");
+// const data = () =>{
+//     fetch('https://raw.githubusercontent.com/DanielaGutierrezG/LIM009-DL-2.0/master/src/data/potter.json')
+//     .then(res=>res.json())
+//     .then(datos=>{
+//         paintData(datos)
+//     })
+// }
+// data();
+
+// const paintData=(datos)=>{
+//     let nuevo="";
+//     for (let i=o; i<datos.lenght; i++){
+//         nuevo+=`
+//        <div> <img src="${datos[i].image} " alt="${datos[i].name}"></img>
+//         </div> 
+//          Nombre:${datos[i].actor} </br>
+//          Tipo: ${datos[i].name} </br> 
+//          Altura: ${datos[i].species} </br>  
+//          Peso: ${datos[i].house} </br>  
+//          Peso: ${datos[i].house} </br>  
+//          Peso: ${datos[i].house} </br>  
+//       </div>
+//       </div>`;
+//     }
+//     contenedor.innerHTML = nuevo;
+//     }
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 /* Manejo del DOM */
+const contenedor = document.getElementById("contenedor");
+const data = () =>{
+    fetch('https://raw.githubusercontent.com/DanielaGutierrezG/LIM009-DL-2.0/master/src/data/potter.json')
+    .then(res=>res.json())
+    .then(datos=>{
+        paintData(datos)
+    })
+}
+data();
+
+const paintData=(datos)=>{
+    let nuevo="";
+    for (let i=0; i<datos.length; i++){
+        nuevo+=`
+        <section class = "col-xs-6 col-sm-6 col-md-4 col-lg-4">
+        <section id = "x" class = "col-xs-12 col-sm-12 col-md-12 col-lg-12"><img id ="Imagenes" src="${datos[i].image}"></img></section> 
+        Actor:${datos[i].actor} </br>
+        Personaje: ${datos[i].name} </br> 
+        Especie: ${datos[i].species} </br>  
+        Casa: ${datos[i].house} </br>   
+        </section> 
+        `;
+    }
+    contenedor.innerHTML = nuevo;
+    }
+
