2d9864537b071f727a213ef05d91e8dcdb66e8c5
DaaDaniela <danieladelcarmen168@hotmail.com>
2019-04-23 22:30:41 -0500

    Cambios

--- test/data.spec.js
+++ test/data.spec.js
@@ -12,3 +12,15 @@ describe('example', () => {
     assert.equal(example(), 'example');
   });
 })
+
+
+// describe('example', () => {
+  
+//   it('debería ser una función', () => {
+//     assert.equal(typeof example, 'function');
+//   });
+
+//   it('debería retornar "example"', () => {
+//     assert.equal(example(), 'example');
+//   });
+// })
